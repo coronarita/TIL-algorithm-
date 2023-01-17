774d59c9819ef8af8d94bed544017269b9acaea0
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2023-01-17 20:30:33 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
@@ -1,5 +1,6 @@
 def solution(array, n):
-    array.sort()
+    array.sort() # 진행할 때, 정렬을 하고 진행하는 것이, hidden case 오답을 방지할 수 있다.
     gap = [abs(i-n) for i in array]
     gap_idx = [i for i in zip(array, gap)]
     return min(gap_idx, key=lambda x : x[1])[0]
+
