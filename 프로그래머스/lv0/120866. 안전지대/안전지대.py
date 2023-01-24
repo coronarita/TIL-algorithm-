afca62e8f302b1db930857d9329d07d12cb19ace
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2023-01-24 22:12:16 +0900

    [level 0] Title: 안전지대, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.py
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.py
@@ -3,41 +3,27 @@ def print_board(board) :
         print(board[i])
 
 def check_mine(board, mine):
-    for i in range(N):
-        for j in range(N):
-            if board[i][j] == 1 :
-                mine.append((i, j))
-    return mine
-            
-def check_safe(board):
-    num_safe = 0
+    num_mine = 0
     for i in range(len(board[0])) :        
         for j in range(len(board[0])):
-            if board[i][j] == 0 :
-                num_safe += 1
+            print(mine[i],i, j)
+            if board[i][j] == 1 :
+                print("mine")
+                
+                mine[i][j] = 1 
+                print(i,j)
         # print(mine[i])
-    return num_safe
-
-def danger(board, x, y) :
-    dx = [-1, 1, 0, 0, -1,-1, 1, 1]
-    dy = [0, 0, -1, 1, -1, 1, -1, 1]
-    for i in range(8):
-        nx = x+dx[i]
-        ny = y+dy[i]
-        if 0 <= nx < N and 0 <= ny < N :
-            board[nx][ny] = 1
-    return board
         
+    return num_mine, mine
+
+    
 def solution(board):
     answer = 0
-    global N
-    N = len(board[0])    
-    mine = []
+    mine = [[0] * len(board[0])]*len(board[0])    
+    print(board[3][2])
     # # test the shape
     # print_board(board)
-    mine = check_mine(board, mine)
-    for x,y in mine :
-        danger(board,x,y)
-    answer = check_safe(board)
     
+    ans, mine = check_mine(board, mine)
+    # print_board(mine)
     return answer
