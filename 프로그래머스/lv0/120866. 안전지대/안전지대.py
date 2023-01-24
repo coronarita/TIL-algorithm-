8f15637eb10fe51e6676267c7bf5f82152bbe447
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2023-01-24 22:46:15 +0900

    [level 0] Title: 안전지대, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.py
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.py
@@ -1,29 +1,49 @@
-def print_board(board) :
-    for i in range(len(board[0])) :
-        print(board[i])
-
-def check_mine(board, mine):
-    num_mine = 0
-    for i in range(len(board[0])) :        
-        for j in range(len(board[0])):
-            print(mine[i],i, j)
-            if board[i][j] == 1 :
-                print("mine")
-                
-                mine[i][j] = 1 
-                print(i,j)
-        # print(mine[i])
+class Mine():
+    def __init__(self, board):
+        self.coord = board
+        self.N = len(board)
         
-    return num_mine, mine
+    def check_mine_position(self):
+        mine_pos = []
+        for x in range(self.N):
+            for y in range(self.N):
+                if self.coord[x][y] == 1 :
+                    mine_pos.append((x,y))
+        return mine_pos
+        
+    def set_bomb(self, mine):
+        # 11, 12, 1
+        # 9, 3
+        # 7, 6, 5
+        dx = [-1, 0, 1, -1, 1, -1, 0, 1]
+        dy = [1, 1, 1, 0, 0, -1, -1, -1]
+        for x, y in mine : 
+            if self.coord[x][y] == 1:
+                for i in range(8):
+                    nx = x + dx[i]
+                    ny = y + dy[i]
+                    # inside the board
+                    if 0 <= nx < self.N and 0<= ny <self.N :
+                        self.coord[nx][ny] = 1
+        
+    def check_board(self):
+        print(self.coord)
+        
+    def count_safezone(self):
+        count = 0
+        for x in range(self.N):
+            for y in range(self.N):
+                if self.coord[x][y] == 0 :
+                    count += 1
+        return count
 
-    
+
 def solution(board):
-    answer = 0
-    mine = [[0] * len(board[0])]*len(board[0])    
-    print(board[3][2])
-    # # test the shape
-    # print_board(board)
-    
-    ans, mine = check_mine(board, mine)
-    # print_board(mine)
-    return answer
+    a = Mine(board)
+    # 지뢰의 좌표 체크
+    mine = a.check_mine_position()
+    # 지뢰 주변에 지뢰 설치
+    a.set_bomb(mine)
+    # a.check_board()    
+    # 지뢰이 설치되지 않은 좌표의 수 세기
+    return a.count_safezone()
