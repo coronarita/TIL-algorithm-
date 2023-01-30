fee38306d1c6f1895d425d3da61ea2dd759aecf9
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2023-01-30 13:44:37 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,34 +1,6 @@
-class line():
-    def __init__(self):
-        self.result = 0
-    
-    def check_cross(self, a, b):
-        l1s, l1e = a[0], a[1]
-        l2s, l2e = b[0], b[1]
-        max_start = max(a[0], b[0])
-        min_end = min(a[1], b[1])
-        # print(max_start, min_end, max_start-min_end)
-        
-        if min_end > max_start :
-            self.result += min_end - max_start
-        
-    def check_triple(self, a,b,c):
-        l1s, l2s, l3s = a[0], b[0], c[0]
-        l1e, l2e, l3e = a[1], b[1], c[1]
-        max_start = max(a[0], b[0], c[0])
-        min_end = min(a[1], b[1], c[1])
-        # print(max_start, min_end, max_start-min_end)
-        if min_end > max_start :
-            self.result -= min_end - max_start
 
 def solution(lines):
-    answer = 0
-    a = line()
-    x, y, z = lines
-    a.check_cross(x,y)
-    a.check_cross(y,z)
-    a.check_cross(z,x)
-    a.check_triple(x,y,z)
-    a.check_triple(x,y,z)
-    return a.result
-
+    sets = []
+    for l in lines: 
+        sets.append(set(range(l[0], l[1])))
+    return len(sets[0]&sets[1] | sets[1]&sets[2] | sets[2]&sets[0])
