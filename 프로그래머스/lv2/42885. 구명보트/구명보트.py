cce3e24447bd2f6c40e8ca8ab1ea136fcafe3902
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2023-02-19 18:26:41 +0900

    [level 2] Title: 구명보트, Time: 9.39 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,17 +1,46 @@
-# using two pointers
+''' using queue'''
 
-def solution(people, limit):
-    answer = 0 
+from collections import deque 
+
+def solution(people, limit) :
+    answer = 0
     people.sort()
-    l, r = 0, len(people) - 1
-
-    while l <= r :
-        if people[l] + people[r] > limit :
-            pass
-        else : 
-            l += 1
-            
-        answer += 1
-        r -= 1
+    dq = deque(people)
     
+    while dq : 
+        if dq[0] + dq[-1] <= limit :
+            dq.popleft()
+        answer += 1
+        if not dq :
+            break
+            
+        dq.pop()
+        
+        
     return answer
+
+
+
+
+
+
+
+
+
+# # using two pointers
+
+# def solution(people, limit):
+#     answer = 0 
+#     people.sort()
+#     l, r = 0, len(people) - 1
+
+#     while l <= r :
+#         if people[l] + people[r] > limit :
+#             pass
+#         else : 
+#             l += 1
+            
+#         answer += 1
+#         r -= 1
+    
+#     return answer
