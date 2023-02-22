30dd2a11e871fc111f52f6d2c16a75b410c8819e
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2023-02-22 21:47:34 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -0,0 +1,14 @@
+import math
+
+def lcm(a, b):
+    return a * b // math.gcd(a, b)
+
+def solution(arr):
+    
+    while len(arr) >= 2 :
+        a = arr.pop()
+        b = arr.pop()
+        arr.append(lcm(a, b))
+        
+    return arr[0]
+    
