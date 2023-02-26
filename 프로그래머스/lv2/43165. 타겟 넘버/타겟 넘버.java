59126a4fe8e852e22753fe9f801d143646401ec9
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2023-02-26 19:29:49 +0900

    [level 2] Title: 타겟 넘버, Time: 5.63 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,31 +1,45 @@
-class Solution {
+// Solution with BFS(Java)
 
-    public int dfs(int prev, int index, int[] numbers, int target) {
-        // condition
-        if (index >= numbers.length){
-            if (target == prev){
-                return 1;
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Solution {
+    
+    class Pair { // define concept of 'Tuple', in python.
+        int cur;
+        int index;
+        
+        Pair(int cur, int index){
+            this.cur = cur;
+            this.index = index;
+        }
+    }
+    
+    public int solution(int[] numbers, int target){
+        int answer = 0;
+        
+        Queue<Pair> queue = new LinkedList<Pair>();
+        queue.offer(new Pair(numbers[0], 0));
+        queue.offer(new Pair(-numbers[0], 0));
+        
+        while (!queue.isEmpty()){
+            Pair p = queue.poll();
+            if (p.index == numbers.length-1){
+                if (p.cur == target){
+                    answer += 1;
+                }
+                continue;
             }
-        return 0;
+            int c1 = p.cur + numbers[p.index+1];
+            int c2 = p.cur - numbers[p.index+1];
+            
+            queue.add(new Pair(c1, p.index+1));
+            queue.add(new Pair(c2, p.index+1));
         }
         
-        int cur1 = prev + numbers[index];
-        int cur2 = prev - numbers[index];
-            
-        int ans = 0;
-        ans += dfs(cur1, index+1, numbers, target);
-        ans += dfs(cur2, index+1, numbers, target);
-        
-        return ans;
-    }
-
-    public int solution(int[] numbers, int target) {
-        int answer = 0;
-        int current = numbers[0];
-        
-        answer += dfs(current, 1, numbers, target);
-        answer += dfs(-current, 1, numbers, target);
         
         return answer;
     }
+    
+    
 }
