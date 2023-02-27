1c6d28e80a62d25708b525ecafa5c327690fb821
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2023-02-27 23:47:29 +0900

    [level 3] Title: 네트워크, Time: 0.45 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,21 +1,17 @@
-def dfs(i, visited, n, computers):
-    from collections import deque
+def dfs(i, visited, n, computers) : 
     visited[i] = 1
     for j in range(n):
         if i!=j and computers[i][j] == 1:
             if visited[j] == 0:
                 dfs(j, visited, n, computers)
 
-
 def solution(n, computers):
-    # initialize
     answer = 0
     visited = [0] * n
     
-    # dfs for all nodes
     for i in range(n):
         if visited[i] == 0 :
             dfs(i, visited, n, computers)
-            answer += 1
+            answer += 1  
     
     return answer
