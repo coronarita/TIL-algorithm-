f1b26f7672c59393d3dba08d373d103f9422bc62
Junghoon Lee <101229388+coronarita@users.noreply.github.com>
2023-02-28 22:17:38 +0900

    [level 3] Title: 가장 먼 노드, Time: 31.56 ms, Memory: 20 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,28 +1,33 @@
+#review
+
 from collections import deque
 
-def solution(n, edge):
+def bfs(graph, v, visited):
+    queue = deque()
+    queue.append(v)
+    
+    while queue :
+        n = queue.popleft()
+        for g in graph[n]:
+            if visited[g] == 0 :
+                visited[g] = visited[n] + 1
+                queue.append(g)
+    
+    return max(visited)
+
+def solution(n, vertex):
+    answer = 0
     graph = [[] for _ in range(n+1)]
     
-    for e in edge:
-        graph[e[0]].append([e[1]])
-        graph[e[1]].append([e[0]])
-        
-    for g in graph:
-        g.sort()
-        
-    visited = [0] * (n+1)
-    def bfs(graph, v, visited):
-        queue = deque()
-        queue.append(v)
-        visited[v] = 1
-        while queue :
-            n = queue.popleft()
-            for i in graph[n] : 
-                if visited[i[0]] == 0 :
-                    visited[i[0]] = visited[n] + 1 # 이전 노드값 보다 1 크게
-                    queue.append(i[0])
-        return max(visited)
+    for v in vertex :
+        graph[v[0]].append(v[1])
+        graph[v[1]].append(v[0])
     
+    for g in graph : 
+        g.sort()
+    # print(graph)
+    visited = [0] * (n+1)
+    visited[1] = 1
     m = bfs(graph, 1, visited)
     
     return visited.count(m)
